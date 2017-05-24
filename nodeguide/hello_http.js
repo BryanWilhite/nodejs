a299c2bab071bfd514d56b44326ec19a407370f1
Bryan Wilhite <bryan.wilhite@gmail.com>
2017-05-23 22:30:01 -0700

    nodeguide.com article

---
+++ nodeguide/hello_http.js
@@ -0,0 +1,9 @@
+//http://nodeguide.com/beginner.html
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello Http');
+});
+server.listen(8080);
