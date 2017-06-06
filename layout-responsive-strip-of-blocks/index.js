d1640e115bab5ae5e1d209538542f83dad47a562
Bryan Wilhite <bryan.wilhite@gmail.com>
2017-06-05 17:09:53 -0700

    added layout-responsive-strip-of-blocks

---
+++ layout-responsive-strip-of-blocks/index.js
@@ -0,0 +1,61 @@
+/*jslint browser: true, this: true, white: true */
+/*global jQuery */
+(function ($) {
+    "use strict";
+    $(function () {
+        var controls = $(".block-container.header > .controls");
+        $(".command.left", controls).click(function (e) {
+            slideBlocks(e, "right");
+        });
+        $(".command.right", controls).click(function (e) {
+            slideBlocks(e, "left");
+        });
+    });
+
+    var slideBlocks = function (event, direction) {
+        //console.log("slideBlocks called...");
+        var header = $(event.target).closest(".block-container.header");
+        var wrapperContainer = header.next(".block-container.wrapper");
+        var wrapperContainerWidth = wrapperContainer.width();
+        var blockWrapper = $("> div", wrapperContainer);
+        var duration = 500;
+        var wrapperLeft = parseInt(blockWrapper.css("left"), 10);
+        var cannotSlideLeft = function () {
+            var fixedBlockWidth = 124;
+            var blocks = $("span", blockWrapper);
+            var totalWidth = fixedBlockWidth * blocks.length;
+            var slideLeftLength = Math.abs(wrapperLeft) + wrapperContainerWidth;
+            return (slideLeftLength >= totalWidth);
+        };
+        var getSlideRightLength = function () {
+            var l = Math.abs(wrapperLeft);
+            return (l > wrapperContainerWidth) ? wrapperContainerWidth : l;
+        };
+        // console.log(
+        //     "blockWrapper:", blockWrapper,
+        //     "wrapperContainer:", wrapperContainer,
+        //     "wrapperContainerWidth:", wrapperContainerWidth,
+        //     "wrapperLeft", wrapperLeft
+        // );
+
+        switch (direction) {
+            case "left":
+                if (cannotSlideLeft()) {
+                    return;
+                }
+                blockWrapper.animate({
+                    left: "-=" + wrapperContainer.width()
+                }, duration);
+                break;
+
+            case "right":
+                if (blockWrapper.position().left >= 0) {
+                    return;
+                }
+                blockWrapper.animate({
+                    left: "+=" + getSlideRightLength()
+                }, duration);
+                break;
+        }
+    };
+}(jQuery));
