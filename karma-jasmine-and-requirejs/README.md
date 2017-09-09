d50ec6ac969459118cf6fe174fa29a251360a297
Bryan Wilhite <bryan.wilhite@gmail.com>
2017-09-09 11:58:18 -0700

    Error: No provider for "framework:jasmine"! (Resolving: framework:jasmine)

--- karma-jasmine-and-requirejs/README.md
+++ karma-jasmine-and-requirejs/README.md
@@ -3,3 +3,5 @@
 [karma-runner.github.io](http://karma-runner.github.io/0.8/plus/RequireJS.html)
 
 [TypeScript modules](http://www.typescriptlang.org/docs/handbook/modules.html)
+
+[karma-typescript-plugin](https://www.npmjs.com/package/karma-typescript-plugin)
--- karma-jasmine-and-requirejs/karma.conf.js
+++ karma-jasmine-and-requirejs/karma.conf.js
@@ -20,11 +20,14 @@ module.exports = function(config) {
         // list of files to exclude
         exclude: [
         ],
-
+        plugins:  [
+            'karma-typescript-plugin',
+            'karma-coverage'
+        ],
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            '**/*.ts': ['typescript']
+            '**/*.ts': ['typescript', 'coverage']
         },
         typescriptPreprocessor: {
             options: {
@@ -39,7 +42,11 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: [
+            'progress',
+            'coverage',
+            'typescript'
+        ],
 
         // web server port
         port: 9876,
