f575eb40e246589deb304377b827397dacee8810
Bryan Wilhite <rasx@kintespace.com>
2017-09-09 22:41:34 -0700

    ts and modules not working

---
+++ typescript-and-modules/src/index.ts
@@ -0,0 +1,7 @@
+import { ZipCodeValidator } from "./zip-code-validator";
+
+const zip = "90045";
+const validator = new ZipCodeValidator();
+const test = validator.isAcceptable(zip);
+
+console.log(`zip ${zip}: ${test}`);
