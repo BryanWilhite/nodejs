0ea4f05a2eed272de6e36a21919c85b2a02ccb1c
Bryan Wilhite <rasx@songhaysystem.com>
2017-09-15 12:51:10 -0700

    tslint

--- karma-and-jasmine-minimal/test/test-subtract.ts
+++ karma-and-jasmine-minimal/test/test-subtract.ts
@@ -1,8 +1,11 @@
 ///<reference path="../node_modules/@types/jasmine/index.d.ts"/>
 ///<reference path="../src/subtract.ts"/>
 
-describe('subtract method', () => {
-    it('subtracts 2 numbers', () => {
+describe("subtract method", () => {
+    let env:jasmine.Env = jasmine.getEnv();
+    console.log(env);
+
+    it("subtracts 2 numbers", () => {
         expect(subtract(2, 4)).toBe(-3);
     });
 });
