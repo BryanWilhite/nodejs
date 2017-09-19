33b022ff1c39d6b3bb1326ffd3d52c446ceb4bd4
Bryan Wilhite <rasx@songhaysystem.com>
2017-09-19 14:16:52 -0700

    added karma-jasmine-jquery framework

--- karma-and-jasmine-minimal/test/jasmine-jquery-intro.spec.ts
+++ karma-and-jasmine-minimal/test/jasmine-jquery-intro.spec.ts
@@ -1,5 +1,23 @@
 ///<reference path="../node_modules/@types/jasmine/index.d.ts"/>
 ///<reference path="../node_modules/@types/jasmine-jquery/index.d.ts"/>
+///<reference path="../node_modules/@types/karma-jasmine/index.d.ts"/>
+/// <reference path="../../typings-jquery/node_modules/@types/jquery/index.d.ts" />
 
-describe("", () => {
+describe("karma-jasmine-jquery framework", () => {
+    jasmine.getFixtures().fixturesPath = "base/served";
+
+    it("has an HTML file", () => {
+        loadFixtures("test1.html");
+
+        let p: JQuery<Element> = <JQuery<Element>>jQuery("#test1");
+        expect(p).not.toBeNull();
+        expect(p.length).toEqual(1);
+        expect(p.text()).toEqual("p");
+
+        let a: JQuery<Element> = <JQuery<Element>>jQuery(".test1");
+        expect(a).not.toBeNull();
+        expect(a.length).toEqual(1);
+        expect(a.attr("href")).toEqual("#");
+    });
+
 });
