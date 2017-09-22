374dee85cea4bf0cc32d888a904c4b11ea3e6122
Bryan Wilhite <rasx@songhaysystem.com>
2017-09-22 15:17:35 -0700

    d3 svg chart working: required svg element not div

--- d3-bostock-bar-charts/index.js
+++ d3-bostock-bar-charts/index.js
@@ -1,7 +1,8 @@
 /// <reference path="../d3-shared/node_modules/@types/d3/index.d.ts" />
 window.onload = function(e) {
     var data = [4, 8, 15, 16, 23, 42];
-    var width = 420, barHeight = 20;
+    var width = 420,
+        barHeight = 20;
 
     var getScale = d3.scaleLinear()
         .domain([0, d3.max(data)])
@@ -17,19 +18,30 @@ window.onload = function(e) {
             return d;
         });
 
-    d3.select(".d3-svg-chart")
+    var chart = d3.select(".d3-svg-chart")
         .attr("width", width)
-        .attr("height", barHeight * data.length)
-        .selectAll("g").data(data) //data join to selection data
-        .enter()
-        .append("g")
-        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; })
-        .append("rect")
-        .attr("width", getScale())
-        .attr("height", barHeight - 1)
-        .append("text")
-        .attr("x", function(d) { return getScale(d) - 3; })
+        .attr("height", barHeight * data.length);
+
+    var bar = chart.selectAll("g")
+        .data(data)
+        .enter().append("g")
+        .attr("transform", function(d, i) {
+            return "translate(0," + i * barHeight + ")";
+        });
+
+    bar.append("rect")
+        .attr("width", function(d) {
+            return getScale(d);
+        })
+        .attr("height", barHeight - 1);
+
+    bar.append("text")
+        .attr("x", function(d) {
+            return getScale(d) - 3;
+        })
         .attr("y", barHeight / 2)
         .attr("dy", ".35em")
-        .text(function(d) { return d; });
+        .text(function(d) {
+            return d;
+        });
 };
