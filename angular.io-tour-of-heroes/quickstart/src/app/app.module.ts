6860c02cfd3a6bb3cfb41404ad97203756100162
Bryan Wilhite <rasx@songhaysystem.com>
2017-10-19 16:47:10 -0700

    added http support

--- angular.io-tour-of-heroes/quickstart/src/app/app.module.ts
+++ angular.io-tour-of-heroes/quickstart/src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './services/in-memory-data.service';
 
 import { AppComponent }         from './components/app.component';
 import { DashboardComponent }   from './components/dashboard/dashboard.component';
@@ -8,12 +15,12 @@ import { HeroDetailComponent }  from './components/hero-detail/hero-detail.compo
 import { HeroesComponent }      from './components/heroes/heroes.component';
 import { HeroService }          from './services/hero.service';
 
-import { AppRoutingModule }     from './app-routing.module';
-
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
--- angular.io-tour-of-heroes/quickstart/src/app/services/hero.service.ts
+++ angular.io-tour-of-heroes/quickstart/src/app/services/hero.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from '../models/hero';
 import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
-    getHero(id: number): Promise<Hero> {
-        return this.getHeroes().then(heroes =>
+    private heroesUrl = 'api/heroes'; // URL to web api
+
+    constructor(private http: Http) {}
+
+    getHeroMock(id: number): Promise<Hero> {
+        return this.getHeroesMock().then(heroes =>
             heroes.find(hero => hero.id === id)
         );
     }
 
-    getHeroes(): Promise<Hero[]> {
+    getHeroesMock(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
     }
 
@@ -20,5 +27,28 @@ export class HeroService {
             // Simulate server latency with 2 second delay
             setTimeout(() => resolve(this.getHeroes()), 2000);
         });
+    }
+
+    getHero(id: number): Promise<Hero> {
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http
+            .get(url)
+            .toPromise()
+            .then(response => response.json().data as Hero)
+            .catch(this.handleError);
+    }
+
+    getHeroes(): Promise<Hero[]> {
+        // http.get() returns an RxJS Observable by design
+        return this.http
+            .get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
     }
 }
