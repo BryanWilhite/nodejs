df32fed7c2eb5e200b0f9fa8c68e3f5657f5128e
Bryan Wilhite <bryan.wilhite@gmail.com>
2018-01-09 20:21:20 -0800

    :pencil: map-object re-factor

--- typescript/src/map-object/main.js
+++ typescript/src/map-object/main.js
@@ -12,13 +12,8 @@ console.log(`map.size: ${map.size}`);
 console.log(`map.get(4): ${map.get(4)}`);
 const json = '{ "1": "one", "2": "two", "3": "three", "4": "four" }';
 const dictionary = JSON.parse(json);
-const parsedMap = new Map();
-for (const i in dictionary) {
-    if (!dictionary.hasOwnProperty(i)) {
-        continue;
-    }
-    console.log(`dictionary[${i}]: ${dictionary[i]}`);
-    parsedMap.set(+i, dictionary[i]);
-}
+const parsedMap = new Map(Object
+    .keys(dictionary)
+    .map(i => [+i, dictionary[+i]]));
 console.log(`parsedMap.size: ${parsedMap.size}`);
 console.log(`parsedMap.get(4): ${parsedMap.get(4)}`);
--- typescript/src/map-object/main.ts
+++ typescript/src/map-object/main.ts
@@ -13,11 +13,11 @@ console.log(`map.get(4): ${map.get(4)}`);
 
 const json = '{ "1": "one", "2": "two", "3": "three", "4": "four" }';
 const dictionary = JSON.parse(json) as { [index: number]: string; };
-const parsedMap = new Map<number, string>();
-for (const i in dictionary) {
-    if (!dictionary.hasOwnProperty(i)) { continue; }
-    console.log(`dictionary[${i}]: ${dictionary[i]}`);
-    parsedMap.set(parseInt(i, 10), dictionary[i]);
-}
+
+const parsedMap = new Map<number, string>(
+    Object
+        .keys(dictionary)
+        .map(i => [+i, dictionary[+i]] as [number, string])
+);
 console.log(`parsedMap.size: ${parsedMap.size}`);
 console.log(`parsedMap.get(4): ${parsedMap.get(4)}`);
