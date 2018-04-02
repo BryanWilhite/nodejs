3d887e65f8416be7cac8eb2baba37d871de1bc60
Bryan Wilhite <rasx@songhaysystem.com>
2018-04-02 14:58:31 -0700

    :microscope: :pencil: added search service test

---
+++ angular.io-tour-of-heroes/quickstart/src/app/services/hero-search.service.spec.ts
@@ -0,0 +1,44 @@
+import { TestBed, getTestBed } from '@angular/core/testing';
+import {
+    BaseRequestOptions,
+    Http,
+    HttpModule,
+    Response,
+    XHRBackend
+} from '@angular/http';
+
+import { HeroSearchService } from './hero-search.service';
+
+describe('HeroSearchService', () => {
+    const testBed: TestBed = getTestBed();
+    let service: HeroSearchService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                BaseRequestOptions,
+                HeroSearchService,
+                {
+                    deps: [XHRBackend, BaseRequestOptions],
+                    provide: Http,
+                    useFactory: (
+                        backend: XHRBackend,
+                        defaultOptions: BaseRequestOptions
+                    ) => new Http(backend, defaultOptions)
+                }
+            ],
+            imports: [HttpModule]
+        });
+    });
+
+    it('should load search results', (done: DoneFn) => {
+        service = testBed.get(HeroSearchService);
+        expect(service).not.toBeNull();
+        service
+            .search('D')
+            .subscribe(value => {
+                console.log(value);
+                done();
+            });
+    });
+});
