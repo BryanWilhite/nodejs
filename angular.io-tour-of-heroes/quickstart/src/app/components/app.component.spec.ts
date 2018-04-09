575e40cedeb566974f2ebad5af1578cb3b9319e4
Bryan Wilhite <bryan.wilhite@cetera.com>
2018-04-09 13:22:34 -0700

    :umbrella: all tests pass

--- angular.io-tour-of-heroes/quickstart/src/app/components/app.component.spec.ts
+++ angular.io-tour-of-heroes/quickstart/src/app/components/app.component.spec.ts
@@ -9,6 +9,8 @@ describe('AppComponent', () => {
     let de: DebugElement;
     let comp: AppComponent;
     let fixture: ComponentFixture<AppComponent>;
+    let routerLinks: RouterLinkMock[];
+    let routerLinkDebugElements: DebugElement[];
 
     beforeEach(
         async(() => {
@@ -20,16 +22,44 @@ describe('AppComponent', () => {
                 .then(() => {
                     fixture = TestBed.createComponent(AppComponent);
                     comp = fixture.componentInstance;
+                    fixture.detectChanges();
+
+                    routerLinkDebugElements = fixture.debugElement.queryAll(
+                        By.directive(RouterLinkMock)
+                    );
+
+                    routerLinks = routerLinkDebugElements.map(i =>
+                        i.injector.get(RouterLinkMock)
+                    );
                 });
         })
     );
 
-    it('should create component', () => expect(comp).toBeDefined());
+    it('should instantiate the component', () => expect(comp).not.toBeNull());
 
     it('should have expected <h1> text', () => {
-        fixture.detectChanges();
         de = fixture.debugElement.query(By.css('h1'));
         const h1 = de.nativeElement;
         expect(h1.innerText).toContain(fixture.componentInstance.title);
+    });
+
+    it('should have routerLink directives', () => {
+        expect(routerLinks.length).toBe(2);
+        expect(routerLinks[0].linkParams).toBe('/dashboard');
+        expect(routerLinks[1].linkParams).toBe('/heroes');
+    });
+
+    it('should have clickable routerLink directives', () => {
+        const i = 1;
+        const linkDebugElement = routerLinkDebugElements[i];
+        const linkDirective = routerLinks[i];
+
+        expect(linkDirective.navigatedTo).toBeNull(
+            'An instance of navigatedTo is not expected.'
+        );
+        linkDebugElement.triggerEventHandler('click', null);
+        fixture.detectChanges();
+
+        expect(linkDirective.navigatedTo).toBe(routerLinks[1].linkParams);
     });
 });
