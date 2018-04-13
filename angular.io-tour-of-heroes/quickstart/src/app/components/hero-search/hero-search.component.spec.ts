fc698780aa502e784ee7f37d06f8b5a5862b9061
Bryan Wilhite <bryan.wilhite@cetera.com>
2018-04-13 10:33:13 -0700

    :microscope: :pencil: setup TestBed

--- angular.io-tour-of-heroes/quickstart/src/app/components/hero-search/hero-search.component.spec.ts
+++ angular.io-tour-of-heroes/quickstart/src/app/components/hero-search/hero-search.component.spec.ts
@@ -5,3 +5,39 @@ import {
     fakeAsync,
     tick
 } from '@angular/core/testing';
+
+import { HeroSearchService } from '../../services/hero-search.service';
+import { ComponentFixtureUtility } from '../../mocks/component-fixture-utility';
+import { HeroSearchComponent } from './hero-search.component';
+import { Router } from '@angular/router';
+
+describe('HeroSearchComponent', () => {
+    const router = jasmine.createSpyObj('Router', ['navigate']);
+    const service = jasmine.createSpyObj('HeroSearchService', ['search']);
+
+    let comp: HeroSearchComponent;
+    let fixture: ComponentFixture<HeroSearchComponent>;
+    let fixtureUtility: ComponentFixtureUtility<HeroSearchComponent>;
+
+    const initializeComponentAndDetectChanges = function() {
+        fixture = TestBed.createComponent(HeroSearchComponent);
+        fixtureUtility = new ComponentFixtureUtility(fixture);
+        comp = fixture.componentInstance;
+
+        fixture.detectChanges(); // triggers ngOnInit which assigns the observable subject
+    };
+
+    beforeEach(
+        async(() => {
+            TestBed.configureTestingModule({
+                providers: [
+                    { provide: Router, useValue: router },
+                    { provide: HeroSearchService, useValue: service }
+                ],
+                declarations: [HeroSearchComponent]
+            })
+                .compileComponents()
+                .then(initializeComponentAndDetectChanges);
+        })
+    );
+});
