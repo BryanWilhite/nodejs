645849844e43ca87ab86a33706f79c4351f34fda
Bryan Wilhite <bryan.wilhite@cetera.com>
2018-04-12 17:51:36 -0700

    :microscope: :hammer: for readability

--- angular.io-tour-of-heroes/quickstart/src/app/components/app.component.spec.ts
+++ angular.io-tour-of-heroes/quickstart/src/app/components/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
 import { NO_ERRORS_SCHEMA, DebugElement } from '@angular/core';
 
+import { ComponentFixtureUtility } from '../mocks/component-fixture-utility';
 import { RouterLinkMock } from '../mocks/directives/router-link-mock';
 import { AppComponent } from './app.component';
 
@@ -9,8 +9,23 @@ describe('AppComponent', () => {
     let de: DebugElement;
     let comp: AppComponent;
     let fixture: ComponentFixture<AppComponent>;
+    let fixtureUtility: ComponentFixtureUtility<AppComponent>;
     let routerLinks: RouterLinkMock[];
     let routerLinkDebugElements: DebugElement[];
+
+    const initializeComponent = function() {
+        fixture = TestBed.createComponent(AppComponent);
+        comp = fixture.componentInstance;
+        fixtureUtility = new ComponentFixtureUtility<AppComponent>(fixture);
+        fixture.detectChanges();
+
+        routerLinkDebugElements = fixtureUtility.queryAllByDirective(
+            RouterLinkMock
+        );
+        routerLinks = routerLinkDebugElements.map(i =>
+            i.injector.get(RouterLinkMock)
+        );
+    };
 
     beforeEach(
         async(() => {
@@ -19,36 +34,21 @@ describe('AppComponent', () => {
                 schemas: [NO_ERRORS_SCHEMA]
             })
                 .compileComponents()
-                .then(() => {
-                    fixture = TestBed.createComponent(AppComponent);
-                    comp = fixture.componentInstance;
-                    fixture.detectChanges();
-
-                    routerLinkDebugElements = fixture.debugElement.queryAll(
-                        By.directive(RouterLinkMock)
-                    );
-
-                    routerLinks = routerLinkDebugElements.map(i =>
-                        i.injector.get(RouterLinkMock)
-                    );
-                });
+                .then(initializeComponent);
         })
     );
 
     it('should instantiate the component', () => expect(comp).not.toBeNull());
-
     it('should have expected <h1> text', () => {
-        de = fixture.debugElement.query(By.css('h1'));
+        de = fixtureUtility.queryByCss('h1');
         const h1 = de.nativeElement;
         expect(h1.innerText).toContain(fixture.componentInstance.title);
     });
-
     it('should have routerLink directives', () => {
         expect(routerLinks.length).toBe(2);
         expect(routerLinks[0].linkParams).toBe('/dashboard');
         expect(routerLinks[1].linkParams).toBe('/heroes');
     });
-
     it('should have clickable routerLink directives', () => {
         const i = 1;
         const linkDebugElement = routerLinkDebugElements[i];
--- angular.io-tour-of-heroes/quickstart/src/app/mocks/component-fixture-utility.ts
+++ angular.io-tour-of-heroes/quickstart/src/app/mocks/component-fixture-utility.ts
@@ -1,17 +1,27 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 export class ComponentFixtureUtility<TComponent> {
-    constructor(protected fixture: ComponentFixture<TComponent>) {}
+    constructor(public fixture: ComponentFixture<TComponent>) {}
 
-    protected query<TElement>(selector: string): TElement {
-        return this.fixture.nativeElement.querySelector(selector);
+    query<TElement>(selector: string): TElement {
+        return this.fixture.nativeElement.querySelector(selector) as TElement;
     }
 
-    protected queryAll<TElement>(selector: string): TElement[] {
+    queryAll<TElement>(selector: string): TElement[] {
         // querySelectorAll() should return a NodeList _not_ an array:
         return Array.from(
             this.fixture.nativeElement.querySelectorAll(selector),
             i => i as TElement
         );
+    }
+
+    queryAllByDirective(directive: any): DebugElement[] {
+        return this.fixture.debugElement.queryAll(By.directive(directive));
+    }
+
+    queryByCss(selector: string): DebugElement {
+        return this.fixture.debugElement.query(By.css(selector));
     }
 }
