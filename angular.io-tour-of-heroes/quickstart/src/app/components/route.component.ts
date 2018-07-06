c6027e3c6cc8eb9fe13d2b1edd7b483a3f13b2e5
Bryan Wilhite <rasx@songhaysystem.com>
2018-07-06 15:49:23 -0700

    :tractor: :rocket: manually added .angular-cli.json to get ng generate working

--- angular.io-tour-of-heroes/quickstart/src/app/components/route.component.ts
+++ angular.io-tour-of-heroes/quickstart/src/app/components/route.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, UrlSegment } from '@angular/router';
 
 @Component({
     selector: 'my-route',
     styleUrls: ['./route.component.css'],
     templateUrl: './route.component.html'
 })
-export class RouteComponent {}
+export class RouteComponent implements OnInit {
+    constructor(private route: ActivatedRoute, private location: Location) {}
+
+    ngOnInit(): void {
+        this.route.url.subscribe((segments: UrlSegment[]) => {
+            // console.log({ segments });
+            if (!segments) {
+                return;
+            }
+            if (!segments.length) {
+                return;
+            }
+
+            const path = segments[0].path;
+            switch (path) {
+                case 'static':
+                    this.location.go('./assets/static.html');
+                    break;
+
+                default:
+                    break;
+            }
+        });
+    }
+}
