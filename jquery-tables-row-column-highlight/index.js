6a3e97c97a04953a97b39d0f0035c7841cabda50
Bryan Wilhite <bryan.wilhite@gmail.com>
2018-11-05 22:44:27 -0800

    :hammer: vs-code format

--- jquery-tables-row-column-highlight/index.js
+++ jquery-tables-row-column-highlight/index.js
@@ -2,39 +2,62 @@
 /*jslint this, white, browser */
 /*global jQuery */
 (function($) {
-    "use strict";
+    'use strict';
 
     $(function() {
-        $("table").data("trIndex", -1).delegate("td", "click", function() {
-            var td = $(this);
-            var tr = td.closest("tr");
-            var table = td.closest("table");
-            var lastColumnIndex = table.data("tdIndex");
-            var lastRowIndex = table.data("trIndex");
-            var currentColumnIndex = td.index() + 1;
-            table.data("trIndex", tr.index() + 1);
-            table.data("tdIndex", currentColumnIndex);
+        $('table')
+            .data('trIndex', -1)
+            .delegate('td', 'click', function() {
+                var td = $(this);
+                var tr = td.closest('tr');
+                var table = td.closest('table');
+                var lastColumnIndex = table.data('tdIndex');
+                var lastRowIndex = table.data('trIndex');
+                var currentColumnIndex = td.index() + 1;
+                table.data('trIndex', tr.index() + 1);
+                table.data('tdIndex', currentColumnIndex);
 
-            if (lastColumnIndex > 0) {
-                $("tbody tr td:nth-child(" + lastColumnIndex + ")", table).removeClass("SelectedColumn");
-                $("thead tr th:nth-child(" + lastColumnIndex + ")", table).removeClass("SelectedColumn");
-            }
+                if (lastColumnIndex > 0) {
+                    $(
+                        'tbody tr td:nth-child(' + lastColumnIndex + ')',
+                        table
+                    ).removeClass('SelectedColumn');
+                    $(
+                        'thead tr th:nth-child(' + lastColumnIndex + ')',
+                        table
+                    ).removeClass('SelectedColumn');
+                }
 
-            if (lastRowIndex > 0) {
-                $("tbody tr:nth-child(" + lastRowIndex + ")", table).removeClass("SelectedRow");
-            }
+                if (lastRowIndex > 0) {
+                    $(
+                        'tbody tr:nth-child(' + lastRowIndex + ')',
+                        table
+                    ).removeClass('SelectedRow');
+                }
 
-            if ((lastColumnIndex > 0) && (lastRowIndex > 0)) {
-                $("tbody tr:nth-child(" + lastRowIndex + ") " +
-                    "td:nth-child(" + lastColumnIndex + ")",
-                table).removeClass("SelectedCell");
-            }
+                if (lastColumnIndex > 0 && lastRowIndex > 0) {
+                    $(
+                        'tbody tr:nth-child(' +
+                            lastRowIndex +
+                            ') ' +
+                            'td:nth-child(' +
+                            lastColumnIndex +
+                            ')',
+                        table
+                    ).removeClass('SelectedCell');
+                }
 
-            $("tbody tr td:nth-child(" + currentColumnIndex + ")", table).addClass("SelectedColumn");
-            $("thead tr th:nth-child(" + currentColumnIndex + ")", table).addClass("SelectedColumn");
+                $(
+                    'tbody tr td:nth-child(' + currentColumnIndex + ')',
+                    table
+                ).addClass('SelectedColumn');
+                $(
+                    'thead tr th:nth-child(' + currentColumnIndex + ')',
+                    table
+                ).addClass('SelectedColumn');
 
-            tr.addClass("SelectedRow");
-            td.removeClass("SelectedColumn").addClass("SelectedCell");
-        });
+                tr.addClass('SelectedRow');
+                td.removeClass('SelectedColumn').addClass('SelectedCell');
+            });
     });
-}(jQuery));
+})(jQuery);
