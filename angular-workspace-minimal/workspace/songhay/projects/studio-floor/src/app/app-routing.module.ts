281f4681db1f2abbc936d82cd64fe8a620982572
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-02-12 16:41:47 -0800

    :tractor: :sparkles: :hammer: rebuilt workspace :books: updated docs

---
+++ angular-workspace-minimal/workspace/songhay/projects/studio-floor/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { CoreComponent } from '@songhay/core';
+
+const routes: Routes = [
+  { path: 'songhay/core/component', component: CoreComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
