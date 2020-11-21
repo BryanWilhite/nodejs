45c77ff4c0f8d6fedf8dd7fe6cb28c9b2f8b8bf8
Bryan Wilhite <rasx@songhaysystem.com>
2020-11-21 10:13:12 -0800

    💄 responsive-layouts

--- responsive-layouts/2020/index/src/ts/index.ts
+++ responsive-layouts/2020/index/src/ts/index.ts
@@ -1,12 +1,26 @@
 import { ajax } from 'rxjs/ajax';
+import { html, render } from 'lit-html';
 
 const uri = './index.json';
 
+interface MyData { title: string; }
+
+function display(parentElement: HTMLElement | null, data: MyData[]): void {
+    if (parentElement === null) {
+        return;
+    }
+
+    const template = html`${data?.map((item) => html`<div>${item.title}</div>`)}`;
+    render(template, parentElement);
+}
+
 window.addEventListener('DOMContentLoaded', () => {
-    const data$ = ajax.getJSON(uri);
+    const data$ = ajax.getJSON<MyData[]>(uri);
+
+    const mainElement = window.document.getElementsByTagName('main')?.item(0);
 
     data$.subscribe(
-        appData => console.log({appData}),
+        appData => display(mainElement, appData),
         err => console.error(err)
     );
 });
