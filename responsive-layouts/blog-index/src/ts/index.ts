272bd605bb3543c4b00dab32ff4a4540d382c62b
Bryan Wilhite <rasx@songhaysystem.com>
2021-01-29 12:20:01 -0800

    responsive-layouts/ namespacing 🔨

--- responsive-layouts/blog-index/src/ts/index.ts
+++ responsive-layouts/blog-index/src/ts/index.ts
@@ -5,40 +5,44 @@ const uri = './index.json';
 
 interface MyData { title: string; thumb: string; body: string; }
 
-function display(parentElement: Element | null | undefined, data: MyData[]): void {
-    if (!parentElement) {
-        return;
-    }
+const rx = {
+    client: {
+        handleContentLoaded: () => {
+            const display = (parentElement: Element | null | undefined, data: MyData[]) => {
+                if (!parentElement) {
+                    return;
+                }
 
-    const getThumbTemplate = (item: MyData) =>
-        html`<span class="thumb">${item.thumb}</span>`;
+                const getThumbTemplate = (item: MyData) =>
+                    html`<span class="thumb">${item.thumb}</span>`;
 
-    const getTitleTemplate = (item: MyData) => html`<h2>${item.title}</h2>`;
-    const getBodyTemplate = (item: MyData) => html`<p>${item.body}</p>`;
+                const getTitleTemplate = (item: MyData) => html`<h2>${item.title}</h2>`;
+                const getBodyTemplate = (item: MyData) => html`<p>${item.body}</p>`;
 
-    const template = html`${
-        data?.map(item =>
-            html`
+                const template = html`${
+                    data?.map(item =>
+                        html`
 <div>${getThumbTemplate(item)}</div>
 <article>
     ${getTitleTemplate(item)}
     ${getThumbTemplate(item)}
     ${getBodyTemplate(item)}
 </article>
-`
-            )
-    }`;
-    render(template, parentElement);
-}
+`)}`;
+                render(template, parentElement);
+            };
 
-window.addEventListener('DOMContentLoaded', () => {
-    const data$ = ajax.getJSON<MyData[]>(uri);
+            const data$ = ajax.getJSON<MyData[]>(uri);
 
-    const mainElement = window.document.getElementsByTagName('main')?.item(0);
-    const mainSectionElement = mainElement?.getElementsByTagName('section')?.item(0);
+            const mainElement = window.document.getElementsByTagName('main')?.item(0);
+            const mainSectionElement = mainElement?.getElementsByTagName('section')?.item(0);
+        
+            data$.subscribe(
+                appData => display(mainSectionElement, appData),
+                err => console.error(err)
+            );
+        }
+    }
+};
 
-    data$.subscribe(
-        appData => display(mainSectionElement, appData),
-        err => console.error(err)
-    );
-});
+window.addEventListener('DOMContentLoaded', () => rx.client.handleContentLoaded());
--- responsive-layouts/index/src/ts/index.ts
+++ responsive-layouts/index/src/ts/index.ts
@@ -1,8 +1,6 @@
 import { ajax } from 'rxjs/ajax';
 import { html, render } from 'lit-html';
 
-const uri = './index.json';
-
 // TODO: use full Document, Segment and IndexEntry models in `songhay`
 interface Document {
     title: string;
@@ -17,45 +15,52 @@ interface IndexEntry extends Partial<Segment> {
     documents?: Partial<Document>[];
 }
 
-function display(parentElement: Element | null | undefined, data: IndexEntry[]): void {
-    if (!parentElement) {
-        return;
+const rx = {
+    client : {
+        handleContentLoaded: () => {
+            const display = (parentElement: Element | null | undefined, data: IndexEntry[]) => {
+                if (!parentElement) {
+                    return;
+                }
+
+                const getArticleTemplate = (childSegment: IndexEntry) => {
+                    return html`<article><h3>${
+                        childSegment.segmentName
+                    }</h3></article>`;
+                };
+
+                const getSectionTemplate = (segment: IndexEntry, index: number) => {
+
+                    const summaryTemplate = html`<summary><h2>${
+                        segment.segmentName
+                    }</h2></summary>`;
+
+                    const sectionTemplate = html`<section>${
+                        segment?.segments?.map(segment => getArticleTemplate(segment))
+                    }</section>`;
+            
+                    const isOpen = index === 0;
+            
+                    return html`<details ?open=${isOpen}>${summaryTemplate}${sectionTemplate}</details>`;
+                }
+
+                const template = html`${data?.map((section, i) => getSectionTemplate(section, i))}`;
+
+                render(template, parentElement);
+            };
+
+            const indexDataUri = './index.json';
+            const data$ = ajax.getJSON<IndexEntry[]>(indexDataUri);
+
+            const mainElement = window.document.getElementsByTagName('main')?.item(0);
+            const mainSectionElement = mainElement?.getElementsByTagName('section')?.item(0);
+
+            data$.subscribe(
+                appData => display(mainSectionElement, appData),
+                err => console.error(err)
+            );
+        }
     }
+};
 
-    const getArticleTemplate = (childSegment: IndexEntry) => {
-        return html`<article><h3>${
-            childSegment.segmentName
-        }</h3></article>`;
-    };
-
-    const getSectionTemplate = (segment: IndexEntry, index: number) => {
-
-        const summaryTemplate = html`<summary><h2>${
-            segment.segmentName
-        }</h2></summary>`;
-
-        const sectionTemplate = html`<section>${
-            segment?.segments?.map(segment => getArticleTemplate(segment))
-        }</section>`;
-
-        const isOpen = index === 0;
-
-        return html`<details ?open=${isOpen}>${summaryTemplate}${sectionTemplate}</details>`;
-    }
-
-    const template = html`${data?.map((section, i) => getSectionTemplate(section, i))}`;
-
-    render(template, parentElement);
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-    const data$ = ajax.getJSON<IndexEntry[]>(uri);
-
-    const mainElement = window.document.getElementsByTagName('main')?.item(0);
-    const mainSectionElement = mainElement?.getElementsByTagName('section')?.item(0);
-
-    data$.subscribe(
-        appData => display(mainSectionElement, appData),
-        err => console.error(err)
-    );
-});
+window.addEventListener('DOMContentLoaded', () => rx.client.handleContentLoaded());
