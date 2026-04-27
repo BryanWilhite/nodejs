df4884815a85d3eee81649f72d542e5815c900d8
Bryan Wilhite <bryan.wilhite@gmail.com>
2026-04-26 20:01:16 -0700

    🖋

--- node-child_process/hello-world-with-args.cs
+++ node-child_process/hello-world-with-args.cs
@@ -1,4 +1,21 @@
 if (args.Length > 0)
-    Console.WriteLine($"Hello {args[0]}!");
+{
+    switch (args[0])
+    {
+        case "do_err":
+            TextWriter errorWriter = Console.Error;
+
+            errorWriter.WriteLine("Doing error!");
+
+            return -1;
+
+        default:
+            Console.WriteLine($"Hello {args[0]}!");
+
+            return 0;
+    }
+}
 else
     Console.WriteLine("Hello world!");
+
+    return 0;
--- node-child_process/hello-world-with-args.js
+++ node-child_process/hello-world-with-args.js
@@ -4,14 +4,19 @@ import { exec } from 'child_process';
 const args = process.argv.slice(2); //because arg-0 is path to the Node.js executable and arg-1 is path to this script file
 
 // Command to run (example: list files)
-exec(`dotnet run --file hello-world-with-args.cs -- ${args[0]}`, (error, stdout, stderr) => {
-    if (error) {
-        console.error(`Error executing command: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.error(`Command error output: ${stderr}`);
-        return;
-    }
-    console.log(`Command output:\n${stdout}`);
-});
+exec(`dotnet run --file hello-world-with-args.cs -- ${args[0]}`,
+    (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing command: ${error.message}`, {error});
+
+            return;
+        }
+
+        if (stderr) {
+            console.error(`Command error output: ${stderr}`, {stderr});
+
+            return;
+        }
+
+        console.log(`Command output:\n${stdout}`);
+    });
