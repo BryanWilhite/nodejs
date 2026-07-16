0cd6c6eda08e7c9f5d48a6425bfbf818762f3f68
Bryan Wilhite <bryan.wilhite@gmail.com>
2026-07-16 14:55:48 -0700

    add new `_index` and`_shared` conventional directories 🚜✨

--- typescript-and-ajax/src/index.ts
+++ typescript-and-ajax/src/index.ts
@@ -8,8 +8,9 @@ const uri = 'https://songhaystorage.blob.core.windows.net/studio-dash/app.json';
 window.addEventListener('DOMContentLoaded', () => {
     const data$ = ajax.getJSON<AppData>(uri);
 
-    data$.subscribe(
-        appData => display(appData),
-        err => console.error(err)
-    );
+    data$.subscribe({
+        next: appData => display(appData),
+        error: (e) => console.error(e),
+        complete: () => console.debug('ajax complete') 
+    });
 });
