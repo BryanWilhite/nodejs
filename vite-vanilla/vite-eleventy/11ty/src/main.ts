89fa79f137c002aa8922cf04eac118d4f7b41cfe
Bryan Wilhite <bryan.wilhite@gmail.com>
2026-07-28 18:30:07 -0700

    generate `/dist` 🚜✨

--- vite-vanilla/vite-eleventy/11ty/src/main.ts
+++ vite-vanilla/vite-eleventy/11ty/src/main.ts
@@ -1,5 +1,16 @@
-import { csv } from 'd3-fetch';
+import { csv } from 'd3-fetch'
 
-const data = await csv('./data/my.csv');
+const data = await csv('./data/my.csv') as [{ID:string,Name:string,Score:number}]
 
-console.table(data);
+const csvTable = window.document.getElementById('csv-table') as HTMLTableElement
+const csvRowTemplate = window.document.getElementById('csv-table-row-template') as HTMLTemplateElement
+
+data.forEach(row => {
+    const rowClone = csvRowTemplate.content.cloneNode(true) as HTMLTableRowElement
+
+    rowClone.querySelector('td:nth-child(1)').textContent = row.ID
+    rowClone.querySelector('td:nth-child(2)').textContent = row.Name
+    rowClone.querySelector('td:nth-child(3)').textContent = `${row.Score}`
+
+    csvTable.tBodies[0].appendChild(rowClone)
+})
