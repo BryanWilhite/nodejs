e526acbeaa4ef8ed8931a205478a76a24070f3e4
Bryan Wilhite <bryan.wilhite@gmail.com>
2026-08-06 21:58:20 -0700

    replace all angular samples with `angular-first-app/` 🚜🔥 🚜⚙✨

--- angular-first-app/src/app/home/home.ts
+++ angular-first-app/src/app/home/home.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HousingLocation } from '../housing-location/housing-location';
+import { HousingLocationInfo } from '../housing-location-info';
+import { HousingService } from '../housing-service';
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  template: `
-    <p>
-      home works!
-    </p>
-  `,
-  styles: ``,
+    selector: 'app-home',
+    imports: [HousingLocation],
+    template: `
+    <section>
+        <form>
+            <input type="text" placeholder="Filter by city" />
+            <button class="primary" type="button">Search</button>
+        </form>
+    </section>
+    <section class="results">
+    @for (housingLocation of housingLocationList; track $index) {
+        <app-housing-location [housingLocation]="housingLocation" />
+    }
+    </section>
+    `,
+    styleUrl: './home.css',
 })
 export class Home {
+    housingLocationList: HousingLocationInfo[] = [];
+    housingService: HousingService = inject(HousingService);
 
+    constructor() {
+        this.housingLocationList = this.housingService.getAllHousingLocations();
+    }
 }
